61470528079419d4ccb1193918ccde29c431eb6c
Peter Kieltyka <peter.kieltyka@gmail.com>
2026-04-15 07:21:04 -0400

    gh pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/ops-4626/' : '/',
   plugins: [react(), tailwindcss()],
-})
+}))
