9a2883de03c949c361e3af113e58de4aac2576a4
PK <peter.kieltyka@gmail.com>
2026-04-16 10:50:26 -0400

    update BASE_PATH

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: process.env.BASE_PATH || '/',
   plugins: [react(), tailwindcss()],
 })
